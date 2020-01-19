08f11081df1b305dc1eb594998768466d13fb44c
Dominik Babić <dominik.babic@codecons.com>
2020-01-19 21:32:44 +0100

    Initial Commit

---
+++ ACLayout/ACLayout.h
@@ -0,0 +1,19 @@
+//
+//  ACLayout.h
+//  ACLayout
+//
+//  Created by Dominik Babić on 19/01/2020.
+//  Copyright © 2020 HeliOs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ACLayout.
+FOUNDATION_EXPORT double ACLayoutVersionNumber;
+
+//! Project version string for ACLayout.
+FOUNDATION_EXPORT const unsigned char ACLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ACLayout/PublicHeader.h>
+
+
---
+++ ACLayoutTests/ACLayoutTests.swift
@@ -0,0 +1,34 @@
+//
+//  ACLayoutTests.swift
+//  ACLayoutTests
+//
+//  Created by Dominik Babić on 19/01/2020.
+//  Copyright © 2020 HeliOs. All rights reserved.
+//
+
+import XCTest
+@testable import ACLayout
+
+class ACLayoutTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
